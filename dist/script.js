dc39e30e4c21795d716579b4000875ab474c10a8
Luis Suizo <95496499+ozius22@users.noreply.github.com>
2023-03-15 18:43:07 +0800

    done with home

--- dist/script.js
+++ dist/script.js
@@ -5,9 +5,16 @@ const navLinks = document.querySelectorAll('nav a').forEach(link => {
   }
 })
 
+const footLinks = document.querySelectorAll('footer a').forEach(link => {
+  if(link.href.includes(`${activePage}`)){
+    link.classList.add('active1');
+  }
+})
 
-/*const typing = document.querySelector('#typing');
+/*
+const typing = document.querySelector('#typing');
 
 typing.addEventListener('animationend', () => {
   typing.classList.add('repeat');
-});*/
+});
+*/
